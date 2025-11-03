36a9a21a17714fa699a069b6000b0c26a0122bc9
Lichao Huang <lhuang38@rrc.ca>
2025-11-02 19:00:23 -0600

    try catch

--- script.js
+++ script.js
@@ -6,3 +6,58 @@ const searchBtn = document.getElementById("search");
 const showcase = document.getElementById("showcase");
 const methodError = document.getElementById("methodError");
 const numberError = document.getElementById("numberError");
+
+searchBtn.addEventListener("click", async () => {
+    //Clear old errors and results
+    methodError.textContent = "";
+    numberError.textContent = "";
+    showcase.innerHTML = "";
+    //Read user input
+    const method = methodSelect.value;
+    const amount = parseInt(amountInput.value);
+    let valid = true;
+
+    if (!method) {
+        methodError.textContent = "Please select a search method.";
+        valid = false;
+    }
+    if (!amountInput.value) {
+    numberError.textContent = "Please enter an amount.";
+    return;
+    }
+
+    showcase.textContent = "Loading";
+
+    try {
+        let soql = "";
+        if (method === "long") {
+        soql = `?$order=length DESC&$limit=${amount}`;
+        } else if (method === "short") {
+        soql = `?$order=length ASC&$limit=${amount}`;
+        } else if (method === "random") {
+        soql = `?$limit=100`;
+        }
+
+        const encodedURL = encodeURI(API_URL + soql);
+        const res = await fetch(encodedURL);
+
+        if (!res.ok) throw new Error(`Error ${res.status}`);
+
+        const data = await res.json();
+
+        if (data.length === 0) {
+        showcase.textContent = "No walkways found.";
+        return;
+        }
+
+        let result = data;
+        if (method === "random") {
+        result = getRandomItems(data, amount);
+        }
+
+        renderWalkways(result, method);
+    } catch (error) {
+        console.error(error);
+        showcase.textContent = "Error loading data.";
+    }
+    });
