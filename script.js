64ebfc6984fdea6370ba3a56afa36ae52a4b7400
Lichao Huang <lhuang38@rrc.ca>
2025-11-02 18:33:42 -0600

    get ID

---
+++ script.js
@@ -0,0 +1,8 @@
+const API_URL = "https://data.winnipeg.ca/resource/jdeq-xf3y.json";
+
+const methodSelect = document.getElementById("Method");
+const amountInput = document.getElementById("amountInput");
+const searchBtn = document.getElementById("search");
+const showcase = document.getElementById("showcase");
+const methodError = document.getElementById("methodError");
+const numberError = document.getElementById("numberError");
